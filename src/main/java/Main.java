976b70fe417abb918e6f070590da70f31fee3937
Suci I Jannah <suci.istachotil@bukalapak.com>
2019-09-15 15:06:45 +0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.suci.math.Operators;
+
+public class Main {
+
+    public static void main (String[] args){
+        //adding operations
+        Operators operators = new Operators();
+        int resultAdd = operators.add(5,6);
+        System.out.println("the result : " + resultAdd);
+
+    }
+}
